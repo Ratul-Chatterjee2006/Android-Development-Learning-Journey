b4fd2898a6086b2ac098b95e7c3e35485b96c1cb
Ratul Chatterjee2006 <ratulc686@gmail.com>
2026-06-24 17:42:15 +0530

    Added Andriod Practice Projects

---
+++ FirstDemoProject/app/src/main/java/com/example/firstdempproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firstdempproject
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonupload=findViewById<Button>(R.id.btnUpload)
+        val buttonDownload=findViewById<Button>(R.id.btnDownload)
+
+        buttonupload.setOnClickListener {
+            Toast.makeText(applicationContext,"Uploading...", Toast.LENGTH_SHORT).show()
+        }
+
+
+        buttonDownload.setOnClickListener {
+            Toast.makeText(applicationContext,"Downloading...", Toast.LENGTH_LONG).show()
+        }
+
+
+        }
+    }
